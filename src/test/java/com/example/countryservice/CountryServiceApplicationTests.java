b6c7312dcf3934b6e026859b7cddf6839181df76
Yazan Barhoush <ybarhoush@gmail.com>
2022-02-03 15:00:54 +0200

    hello world setup

---
+++ src/test/java/com/example/countryservice/CountryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
