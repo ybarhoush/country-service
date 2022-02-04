ec2f4d87244e29342f9aaf67bae155b6fb6eb2ea
Yazan Barhoush <ybarhoush@gmail.com>
2022-02-04 18:55:05 +0200

    add Spring Data and H2 database dependencies, create CountryRepository.java and CountryController.java

---
+++ src/main/java/com/example/countryservice/Controllers/CountryController.java
@@ -0,0 +1,33 @@
+package com.example.countryservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import com.example.countryservice.entities.Country;
+import com.example.countryservice.repositories.CountryRepository;
+
+import java.lang.Iterable;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+  private final CountryRepository countryRepository;
+
+  public CountryController(CountryRepository countryRepository) {
+    this.countryRepository = countryRepository;
+  }
+
+  @GetMapping()
+  public Iterable<Country> getAllCountries() {
+    Iterable<Country> countries = this.countryRepository.findAll();
+    return countries;
+  }
+
+}
--- src/main/java/com/example/countryservice/entities/Country.java
+++ src/main/java/com/example/countryservice/entities/Country.java
@@ -1,7 +1,15 @@
 package com.example.countryservice.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Country {
 
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
   private String name;
   private String countryCode;
